73b38cfa4fbe7be225dfa785c7f03cd602b00187
lorlouis <louis.svengoulet@gmail.com>
2020-07-30 12:34:29 -0400

    fixed a bunch of stuff, I guess I am too tired

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ echo "This config only works for neovim"
 echo "Trying to use it with regular vim will generate a few error"
 echo "If you want to fix these errors and make it truly portable"
 echo "send a merge request to https://github.com/lorlouis/dotfiles"
-
-mkdir -p /home/"$USER"/.config/nvim/ftdetect
-cp ./home/config/nvim/ftdetect/* /home/"$USER"/.config/nvim/ftdetect/
+echo ""
+mkdir -p /home/"$USER"/.config/nvim/ftdetect/
+cp -f ./home/config/nvim/ftdetect/* /home/"$USER"/.config/nvim/ftdetect/
 mkdir -p /home/"$USER"/.config/nvim/syntax
-cp ./home/config/nvim/syntax/* /home/"$USER"/.config/nvim/syntax/
-cp -f ./home/config/nvim/init.vim /home/"$USER"/.congig/nvim/init.vim
+cp -f ./home/config/nvim/syntax/* /home/"$USER"/.config/nvim/syntax/
+cp -f ./home/config/nvim/init.vim /home/"$USER"/.config/nvim/init.vim
 
 mkdir -p /home/"$USER"/Templates
 cp ./home/Templates/* /home/"$USER"/Templates/
@@ -16,7 +16,7 @@ cp ./home/Templates/* /home/"$USER"/Templates/
 mkdir -p /home/"$USER"/.vim/ftdetect
 cp ./home/vim/ftdetect/* /home/"$USER"/.vim/ftdetect/
 mkdir -p /home/"$USER"/.vim/syntax
-cp ./home/vim/syntac/* /home/"$USER"/.vim/syntac/
+cp ./home/vim/syntax/* /home/"$USER"/.vim/syntax/
 cp -f ./home/vimrc /home/"$USER"/.vimrc
 
 echo "Attempting to copy py_kw to /usr/share/dict/py_ky"
